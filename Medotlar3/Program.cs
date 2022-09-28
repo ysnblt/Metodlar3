34e1e513e23f246201e14f2741bcb7d292358e53
ysnblt <106676214+ysnblt@users.noreply.github.com>
2022-09-28 20:34:41 +0300

    Add files via upload

---
+++ Medotlar3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medotlar3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // girilen sayının tek mi çift mi olduğunu yazdıran program
+            Console.WriteLine("Bir sayı giriniz");
+            int girilenDeger = Convert.ToInt32(Console.ReadLine());
+            TekmiCiftmi(girilenDeger);
+            Console.ReadLine();
+        }
+
+        static void TekmiCiftmi(int sayi)
+        {
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine("Sayı çifttir");
+            }
+            else
+            {
+                Console.WriteLine("Sayı tektir");
+            }
+        }
+    }
+
+ 
+}
---
+++ Medotlar3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Medotlar3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Medotlar3")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("5a35190f-c63e-41b8-82f1-7bd37661029b")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
